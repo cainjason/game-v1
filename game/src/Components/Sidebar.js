91d1ba40b33d27705dbc4c365b42f9378043bc90
Jason Cain <118203097+cainjason@users.noreply.github.com>
2023-11-14 22:08:31 -0500

    styled Sidebar Header

--- game/src/Components/Sidebar.js
+++ game/src/Components/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
+import CreateIcon from '@material-ui/icons/Create';
 
 function Sidebar() {
   return (
@@ -13,6 +14,7 @@ function Sidebar() {
                 Jason Cain
             </h3>
         </SidebarInfo>
+        <CreateIcon />
       </SidebarHeader>
     </SidebarContainer>
   )
@@ -24,11 +26,29 @@ const SidebarContainer = styled.div`
     color: white;
     background-color: gray;
     flex: 0.3;
-    border-top: 1px solid gray;
+    border-top: 1px solid #49274b;
     max-width: 260px;
     margin-top: 65px;
+    box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 6);
 `;
 
-const SidebarHeader = styled.div``;
+const SidebarHeader = styled.div`
+    display: flex;
+    border-bottom: 1px solid #49274b;
+    padding: 13px;
+
+    > .MuiSvgIcon-root {
+        padding: 8px;
+        color: gray;
+        font-size: 50px;
+        background-color: white;
+        border-radius: 999px;
+        margin-left: 10px;
+        cursor: pointer;
+        :hover {
+            opacity: 0.8;
+        }
+    }
+`;
 
 const SidebarInfo = styled.div``;
